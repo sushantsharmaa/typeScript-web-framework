c59a695a38c5a53a2b1cf62458632fcd1431b242
sushantdemlo <131941348+sushantdemlo@users.noreply.github.com>
2024-05-14 13:41:31 +0530

    save method init

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
 import { User } from './models/User';
 
-const user = new User({ id: 1 });
+const user = new User({ name: 'Sushant', age: 28 });
 
-user.fetch();
-
-setTimeout(() => {
-  console.log(user);
-}, 3000);
+user.save();
